3c275582cb3f3bab5102dbc15a80ec3644ba7365
Lucas Zarate(MasterPolom22) <91094440+MasterPolom22@users.noreply.github.com>
2025-08-30 21:08:59 -0300

    sd
    sds

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Inmobiliaria_Zarate_DoNet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
